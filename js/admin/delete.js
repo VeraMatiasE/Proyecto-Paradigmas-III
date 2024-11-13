cfbb24ab6343c52ac4966efec76544bde588397d
Matias Vera <matiasezequielvera@gmail.com>
2024-11-13 14:50:01 -0300

    Agregado la eliminación lógica

--- js/admin/delete.js
+++ js/admin/delete.js
@@ -1,32 +1,35 @@
-async function deleteMission(id_mission) {
-    if (confirm("¿Estás seguro de que deseas eliminar esta misión?")) {
-        try {
-            const response = await fetch('../../api/missions/delete_mission.php', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ id_mission })
-            });
-
-            const result = await response.json();
-
-            if (result.success) {
-                alert("La misión se ha eliminado con éxito.");
-                document.getElementById(`mission-${id_mission}`).remove();
-            } else {
-                alert("Hubo un problema al eliminar la misión.");
+document.addEventListener("DOMContentLoaded", () => {
+    async function deleteEntity(entityId, entityType) {
+        if (confirm(`¿Estás seguro de que deseas eliminar este/a ${entityType}?`)) {
+            try {
+                const response = await fetch('../../api/delete_entity.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ id: entityId, type: entityType })
+                });
+    
+                const result = await response.json();
+    
+                if (result.success) {
+                    alert(`${entityType} eliminado(a) con éxito.`);
+                    document.getElementById(`${entityType}-${entityId}`).remove();
+                } else {
+                    alert(`Hubo un problema al eliminar el/la ${entityType}.`);
+                }
+            } catch (error) {
+                console.error('Error al eliminar:', error);
+                alert("Error en el servidor al intentar eliminar.");
             }
-        } catch (error) {
-            console.error('Error al eliminar la misión:', error);
-            alert("Error en el servidor al intentar eliminar la misión.");
         }
     }
-}
-
-document.querySelectorAll('.delete-mission-btn').forEach(button => {
-    button.addEventListener('click', (event) => {
-        const id_mission = button.getAttribute('data-id');
-        deleteMission(id_mission);
+    document.querySelectorAll('.delete-button').forEach(button => {
+        button.addEventListener('click', (event) => {
+            const entityId = button.getAttribute('data-id');
+            const entityType = button.getAttribute('data-type');
+            deleteEntity(entityId, entityType);
+        });
     });
-});
+    
+})
